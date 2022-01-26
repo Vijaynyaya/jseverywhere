8136cc0a65a577a91994f14d4fbe9d8072e44c2b
Vijay Kumar <vijaynyaya@gmail.com>
2022-01-26 12:39:12 +0530

    add Note model (ODM)

---
+++ src/models/note.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+// define note's database schema
+const noteSchema = new mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        // Assign createdAt and updatedAt fields with a Date type
+        timestamps: true
+    }
+)
+
+// use the schema to define the 'Note' model
+const Note = mongoose.model('Note', noteSchema)
+
+module.exports = Note;
