53cf0f184907fcc7e44d9e447cabec649fb2f20d
Vijay Kumar <vijaynyaya@gmail.com>
2022-01-26 09:10:46 +0530

    initialize express app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // index.js
 // This is the main entry point of our application
+const express = require("express")
+const app = express() // initialize expresss
+
+// equivalend of defining a route
+app.get('/', (request, response) => response.send("Hello, JS Everywhere!"))
+
+// run the app on port 4000
+app.listen(4000, () => console.log("Listening  on port 4000"))
