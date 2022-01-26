797a09e7fe07ab702052eab272815cf63d3c2d3f
Vijay Kumar <vijaynyaya@gmail.com>
2022-01-26 10:53:03 +0530

    add Notes

---
+++ src/demo-data.js
@@ -0,0 +1,7 @@
+module.exports = {
+    notes: [
+        { id: '1', content: 'Republic Day on 26th January', author: 'Mr. Desai' },
+        { id: '2', content: 'Fixed meeting on 27th at 12:30 pm', author: 'HR' },
+        { id: '3', content: 'Je ne regrette rien', author: 'Edith Paif' }
+    ]
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const express = require("express")
 const process = require("process") // not required but recommended by Node Docs
 const { ApolloServer, gql } = require("apollo-server-express") // ES6 object destructuring
 
+const { notes: NOTES } = require("./demo-data")  // ES6 name while destructuring objects
+
 // load environment variables from .env file into process.env
 require('dotenv').config()
 
@@ -12,14 +14,21 @@ const port = process.env.PORT || 4000
 
 // define a schema, using GraphQl's Domain Specific Language(DSL) for schemas
 const typeDefs = gql`
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+    }
     type Query {
-        hello: String
+        hello: String!
+        notes: [Note!]!
     }
 `; // ES6 tagged template literal
 // define resolver functions for fields defined in GraphQl schema
 const resolvers = {
     Query: {
-        hello: () => "Hello, GraphQL API!",
+        hello: () => "Hello, GraphQL API!", // implicit return
+        notes: () => NOTES
     }
 }
 
