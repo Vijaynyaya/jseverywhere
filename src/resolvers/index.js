25131916d52e89c79d47e635a34c9e6cf1220c86
Vijay Kumar <vijaynyaya@gmail.com>
2022-01-26 19:28:46 +0530

    add nested resolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,9 +1,13 @@
 const Mutation = require("./mutation")
 const Query = require("./query")
+const Note = require("./note")
+const User = require("./user")
 const { GraphQLDateTime } = require("graphql-iso-date")
 
 module.exports = {
     Mutation,
     Query,
+    User,
+    Note,
     DateTime: GraphQLDateTime
 }
